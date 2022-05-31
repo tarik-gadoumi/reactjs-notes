bef266e823dffbd67f569cd25dbea8252b1e320b
tarik-gadoumi <tarik.gadoumi@laplateforme.io>
2022-05-31 13:36:27 +0200

    useRef

---
+++ level-18/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+function UsernameForm({ onSubmitDiyalLform }) {
+  const Tracker = React.useRef();
+  const [msg, setMsg] = React.useState();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const { value } = Tracker.current;
+    onSubmitDiyalLform(value);
+  }
+  function watcherFn(event) {
+    const { value } = Tracker.current;
+    const isLowerCase = value === value.toLowerCase();
+    setMsg(isLowerCase ? null : 'Letters must be in Lower Case');
+  }
+
+  return (
+    <div
+      style={{ display: 'flex', textAlign: 'center', justifyContent: 'center' }}
+    >
+      <form
+        style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={handleSubmit}
+      >
+        <label>what's your name</label>
+        <input onChange={watcherFn} ref={Tracker} name="foo"></input>
+        <div
+          style={{
+            fontWeight: 'bold',
+            color: 'black',
+            background:
+              'radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)',
+          }}
+        >
+          {msg}
+        </div>
+        <button disabled={Boolean(msg)}>Cliki me !</button>
+      </form>
+    </div>
+  );
+}
+
+function App() {
+  const affichageAlerte = (name) => alert(`Hello dear ${name}`);
+  return <UsernameForm onSubmitDiyalLform={affichageAlerte} />;
+}
+export default App;
