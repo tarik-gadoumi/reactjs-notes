689d8531dc1c54e6ab1d6bd447f2be14fea9c62e
tarik-gadoumi <tarik.gadoumi@laplateforme.io>
2022-06-05 20:26:42 +0200

    *

--- level-50/App.js
+++ level-50/App.js
@@ -22,8 +22,13 @@ import {
  */
 function useSafeDispatch(dispatch) {
   const isMounted = React.useRef(false);
+  // to make this even more generic you should use the useLayoutEffect hook to
+  // make sure that you are correctly setting the mountedRef.current immediately
+  // after React updates the DOM. Even though this effect does not interact
+  // with the dom another side effect inside a useLayoutEffect which does
+  // interact with the dom may depend on the value being set
   React.useLayoutEffect(
-    // ou useEffect
+    // ou useEffect mais le mieux useLayoutEffect
     () => {
       isMounted.current = true;
       return () => {
